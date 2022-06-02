d7d9c997a84a8408fd5d87ef24b43a04838100fa
harshadsutar <harshadsutar007@gmail.com>
2022-06-02 20:39:05 +0530

    Hashed Password

--- hello.py
+++ hello.py
@@ -1,12 +1,14 @@
 import email
 from enum import unique
+from wsgiref.validate import validator
 from flask import Flask, render_template, flash, request
 from flask_wtf import FlaskForm
-from wtforms import StringField, SubmitField
-from wtforms.validators import DataRequired
+from wtforms import StringField, SubmitField, PasswordField, BooleanField, ValidationError
+from wtforms.validators import DataRequired, EqualTo, Length
 from flask_migrate import Migrate
 from flask_sqlalchemy import SQLAlchemy
 from datetime import datetime
+from werkzeug.security import generate_password_hash, check_password_hash
 
 
 app = Flask(__name__)
@@ -30,7 +32,21 @@ class Users(db.Model):
     email=db.Column(db.String(120),nullable=False,unique=True)
     favorite_color = db.Column(db.String(120))
     date_added=db.Column(db.DateTime, default=datetime.utcnow)
+    password_hash =db.Column(db.String(128))
 
+    @property
+    def password(self):
+        raise AttributeError('password is not readable')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password_hash(self,password):
+        return check_password_hash(self.password_hash, password)
+
+    
     def __repr__(self):
         return '<Name %r>' % self.name
 
@@ -39,8 +55,33 @@ class UserForm(FlaskForm):
     name = StringField("Name", validators=[DataRequired()])
     email = StringField("Email", validators=[DataRequired()])
     favorite_color = StringField("Favorite Color")
+    password_hash = PasswordField('Password', validators=[DataRequired(), EqualTo('password_hash2', message='Passwords Must Match')])
+    password_hash2 = PasswordField('Confirm Password', validators=[DataRequired()])
     submit = SubmitField('Submit')
 
+@app.route('/delete/<int:id>')
+def delete(id):
+    user_to_delete = Users.query.get_or_404(id)
+    name = None
+    form = UserForm()
+
+    try:
+        db.session.delete(user_to_delete)
+        db.session.commit()
+        flash("User Deleted Successfully!!")
+
+        our_users = Users.query.order_by(Users.date_added)
+        return render_template("add_user.html",
+        form=form,
+        name=name,
+        our_users=our_users)
+    except:
+        flash("Whoops! There was a problem while deleting")
+        return render_template("add_user.html",
+        form=form,
+        name=name,
+        our_users=our_users,
+        )
 
 
 @app.route('/update/<int:id>' , methods=['GET','POST'])
@@ -57,7 +98,7 @@ def update(id):
             flash("User Updated Successfully!")
             return render_template("update.html" ,
             form=form,
-            name_to_update = name_to_update)
+            name_to_update = name_to_update,id = id)
         except:
             flash("Error! Looks like something went went wrong")
             return render_template("update.html" ,
@@ -66,7 +107,8 @@ def update(id):
     else:
          return render_template("update.html" ,
             form=form,
-            name_to_update = name_to_update)
+            name_to_update = name_to_update,
+            id = id)
 
 
 class NamerForm(FlaskForm):
@@ -112,13 +154,15 @@ def add_user():
     if form.validate_on_submit():
             user = Users.query.filter_by(email=form.email.data).first()
             if user is None:
-                user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data)
+                hashed_pw = generate_password_hash(form.password_hash.data, "sha256")
+                user = Users(name=form.name.data, email=form.email.data, favorite_color=form.favorite_color.data, password_hash=hashed_pw)
                 db.session.add(user)
                 db.session.commit()
             name = form.name.data
             form.name.data = ''
             form.email.data = ''
             form.favorite_color.data = ''
+            form.password_hash.data = ''
             flash("User Added Successfull")
     our_users = Users.query.order_by(Users.date_added)
     return render_template("add_user.html",
