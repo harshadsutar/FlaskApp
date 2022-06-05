d96614bf71c4e6eead36b8e0dfe63a6a1f86b898
harshadsutar <harshadsutar007@gmail.com>
2022-06-05 14:32:42 +0530

    added dashboard

--- hello.py
+++ hello.py
@@ -70,13 +70,6 @@ def logout():
     return redirect(url_for('login'))
 
 
-class Posts(db.Model):
-    id = db.Column(db.Integer, primary_key=True)
-    title = db.Column(db.String(255))
-    content = db.Column(db.Text)
-    author = db.Column(db.String(255))
-    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
-    slug = db.Column(db.String(255))
 
 # create posts form
 
@@ -88,6 +81,7 @@ class PostForm(FlaskForm):
     submit = SubmitField("Submit", validators=[DataRequired()])
 
 @app.route('/add-post' , methods=['GET','POST'])
+@login_required
 def add_post():
     form = PostForm()
 
@@ -119,6 +113,7 @@ def post(id):
 
 
 @app.route('/posts/edit/<int:id>',methods=['GET','POST'])
+@login_required
 def edit_post(id):
     post = Posts.query.get_or_404(id)
     form = PostForm()
@@ -158,30 +153,7 @@ def delete_post(id):
 
    
 
-class Users(db.Model, UserMixin):
-    id = db.Column(db.Integer, primary_key=True)
-    username = db.Column(db.String(20),nullable=False,unique=True)
-    name= db.Column(db.String(200), nullable=False)
-    email=db.Column(db.String(120),nullable=False,unique=True)
-    favorite_color = db.Column(db.String(120))
-    date_added=db.Column(db.DateTime, default=datetime.utcnow)
-    password_hash =db.Column(db.String(128))
 
-    @property
-    def password(self):
-        raise AttributeError('password is not readable')
-
-    @password.setter
-    def password(self, password):
-        self.password_hash = generate_password_hash(password)
-
-
-    def verify_password_hash(self,password):
-        return check_password_hash(self.password_hash, password)
-
-    
-    def __repr__(self):
-        return '<Name %r>' % self.name
 
 
 class UserForm(FlaskForm):
@@ -347,6 +319,43 @@ def add_user():
     form=form,
     name=name,
     our_users=our_users)
+
+
+
+
+class Posts(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    title = db.Column(db.String(255))
+    content = db.Column(db.Text)
+    author = db.Column(db.String(255))
+    date_posted = db.Column(db.DateTime, default=datetime.utcnow)
+    slug = db.Column(db.String(255))
+
+class Users(db.Model, UserMixin):
+    id = db.Column(db.Integer, primary_key=True)
+    username = db.Column(db.String(20),nullable=False,unique=True)
+    name= db.Column(db.String(200), nullable=False)
+    email=db.Column(db.String(120),nullable=False,unique=True)
+    favorite_color = db.Column(db.String(120))
+    date_added=db.Column(db.DateTime, default=datetime.utcnow)
+    password_hash =db.Column(db.String(128))
+
+    @property
+    def password(self):
+        raise AttributeError('password is not readable')
+
+    @password.setter
+    def password(self, password):
+        self.password_hash = generate_password_hash(password)
+
+
+    def verify_password_hash(self,password):
+        return check_password_hash(self.password_hash, password)
+
+    
+    def __repr__(self):
+        return '<Name %r>' % self.name
+
 
 if __name__ == "__main__":
     app.run()
